89cb86331000f7d4d040b7eefe985eaf163eabc2
anoff <offenhaeuser@gmail.com>
2019-02-17 23:11:35 +0100

    :star2: feat: add cookie consent

---
+++ static/init-cookieconsent.js
@@ -0,0 +1,13 @@
+// https://cookieconsent.insites.com/download/#
+window.addEventListener('load', function () {
+  window.cookieconsent.initialise({
+    'palette': {
+      'popup': {
+        'background': '#cc0033'
+      },
+      'button': {
+        'background': '#fff'
+      }
+    }
+  })
+})
