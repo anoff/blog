0a60f3d773bffd808fd2ab852b3428ddcd7f6ac6
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2020-07-22 21:33:00 +0200

    :construction_worker: build: use working docker image to build asciidoc

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -v $PWD:/app anoff/hugo-asciidoctor:latest hugo --gc --minify -d _site -b //localhost:5000
+docker run --rm -v $PWD:/app anoff/hugo-asciidoctor:1.0 hugo --gc --minify -d _site -b //localhost:5000
